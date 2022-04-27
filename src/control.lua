8c383424f8862d4ad86e296675f860e8071f81ed
Simon Gehring <simon@gehring.tv>
2022-04-27 20:34:07 +0200

    Added setting for darkness

--- src/control.lua
+++ src/control.lua
@@ -1,15 +1,12 @@
 
-local function setup()
-
+local function setDarkness()
+    local darkness = settings.global["necessary-lights-darkness"].value
     local surface = game.surfaces['nauvis']
-    surface.brightness_visual_weights = { 1 / 0.85, 1 / 0.85, 1 / 0.85 }
+    surface.brightness_visual_weights = { darkness, darkness, darkness }
     surface.min_brightness = 0
-    surface.daytime = 0.5 -- midnight
-    surface.freeze_daytime = true
 end
 
-local function init()
-    setup()
-end
 
-script.on_init(init)
+script.on_init(setDarkness)
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, setDarkness)
---
+++ src/settings.lua
@@ -0,0 +1,15 @@
+data:extend(
+        {
+            {
+                type = "double-setting",
+                name = "necessary-lights-darkness",
+                default_value = 0.15,
+                minimum_value = 0,
+                maximum_value = 1,
+                order = 1,
+                setting_type = "runtime-global",
+                localised_name = "Darkness",
+                localised_description = "Alters the amount of darkness your experience. 0 being normal and 1 being pitch black."
+            }
+        }
+)
