edeb43bb926d42f80f8c2b06c0bed513e5f3ba44
Simon Gehring <simon@gehring.tv>
2022-05-07 12:00:55 +0200

    Added more options

--- src/data.lua
+++ src/data.lua
@@ -1,7 +1,11 @@
+minimum_darkness = settings.startup["necessary-lights-player-minimum-darkness"].value
+player_lightning_intensity = settings.startup["necessary-lights-player-intensity"].value
+player_lightning_size = settings.startup["necessary-lights-player-size"].value
+
 data.raw["character"]["character"].light = {
     {
-        minimum_darkness = 0.3,
-        intensity = 0.20,
-        size = 30,
+        minimum_darkness = minimum_darkness,
+        intensity = player_lightning_intensity,
+        size = player_lightning_size,
     }
 }
--- src/settings.lua
+++ src/settings.lua
@@ -10,6 +10,39 @@ data:extend(
                 setting_type = "runtime-global",
                 localised_name = "Darkness",
                 localised_description = "Alters the amount of darkness your experience. 0 being normal and 1 being pitch black."
-            }
+            },
+            {
+                type = "double-setting",
+                name = "necessary-lights-player-minimum-darkness",
+                default_value = 0.3,
+                minimum_value = 0,
+                maximum_value = 1,
+                order = 2,
+                setting_type = "startup",
+                localised_name = "Character Light (Minimum Darkness)",
+                localised_description = ""
+            },
+            {
+                type = "double-setting",
+                name = "necessary-lights-player-intensity",
+                default_value = 0.2,
+                minimum_value = 0,
+                maximum_value = 1,
+                order = 3,
+                setting_type = "startup",
+                localised_name = "Character Light Intensity",
+                localised_description = ""
+            },
+            {
+                type = "int-setting",
+                name = "necessary-lights-player-size",
+                default_value = 30,
+                minimum_value = 0,
+                maximum_value = 2000,
+                order = 4,
+                setting_type = "startup",
+                localised_name = "Character Light Size",
+                localised_description = ""
+            },
         }
 )
